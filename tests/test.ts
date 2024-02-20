3ff2c10586989237ada525c00e8b7526489ce514
denmigda <denis.migdal@uca.fr>
2024-02-20 10:28:37 +0100

    Add test with JSDOM

---
+++ tests/test.ts
@@ -0,0 +1,32 @@
+import { JSDOM } from "jsdom";
+
+const {window} = new JSDOM(
+  `<!DOCTYPE html>
+  <html lang="en">
+    <body>
+    </body>
+  </html>`,
+)!;
+
+globalThis.document = window.document;
+globalThis.customElements = window.customElements;
+
+for(let key of Object.getOwnPropertyNames(window))
+	if(key.startsWith("HTML") )
+		(globalThis as any)[key] = window[key];
+
+let LISS = (await import(`LISS`)).default;
+
+Deno.test("Name", async () => {
+
+	class MyComponent extends LISS({host: HTMLDivElement}) {}
+
+	// define the "my-component" component.
+	LISS.define('my-component', MyComponent);
+
+	let elem = await LISS.build('my-component');
+
+	if( elem.host.getAttribute("is") !== "my-component") {
+		throw new Error("miss");
+	}
+})
