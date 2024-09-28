f6a44868a4575789138910b9307969e2b85e5c74
denmigda <denis.migdal@uca.fr>
2024-09-28 16:57:23 +0200

    Remove build()

--- src/helpers/build.ts
+++ src/helpers/build.ts
@@ -33,14 +33,14 @@ export function lissSync<T extends LISSBase>(str: readonly string[], ...args: an
 }
 
 
-type BUILD_OPTIONS<T extends LISSBase<any,any,any,any>> = Partial<{
-    params    : Partial<inferParams<T>>,
+type BUILD_OPTIONS<T extends LISSBase> = Partial<{
+    params    : Partial<T["params"]>,
     content	  : string|Node|readonly Node[],
-  id 		  : string,
+    id 		    : string,
     classes	  : readonly string[],
     cssvars   : Readonly<Record<string, string>>,
     attrs 	  : Readonly<Record<string, string|boolean>>,
-    data 	  : Readonly<Record<string, string|boolean>>,
+    data 	    : Readonly<Record<string, string|boolean>>,
     listeners : Readonly<Record<string, (ev: Event) => void>>
 }> & ({
   initialize: false,
@@ -50,74 +50,75 @@ type BUILD_OPTIONS<T extends LISSBase<any,any,any,any>> = Partial<{
   parent?: Element
 });
 
-async function build<T extends keyof Components>(tagname: T, options?: BUILD_OPTIONS<Components[T]>): Promise<Components[T]>;
-async function build<T extends LISSBase<any,any,any,any>>(tagname: string, options?: BUILD_OPTIONS<T>): Promise<T>;
-async function build<T extends LISSBase<any,any,any,any>>(tagname: string, {
-params    = {},
-initialize= true,
-content   = [],
-parent    = undefined,
-id 		  = undefined,
-classes   = [],
-cssvars   = {},
-attrs     = {},
-data 	  = {},
-listeners = {}
-}: BUILD_OPTIONS<T> = {}): Promise<T> {
+//async function build<T extends keyof Components>(tagname: T, options?: BUILD_OPTIONS<Components[T]>): Promise<Components[T]>;
+/*
+async function build<T extends LISSBase>(tagname: string, options?: BUILD_OPTIONS<T>): Promise<T>;
+async function build<T extends LISSBase>(tagname: string, {
+              params    = {},
+              initialize= true,
+              content   = [],
+              parent    = undefined,
+              id 		  = undefined,
+              classes   = [],
+              cssvars   = {},
+              attrs     = {},
+              data 	  = {},
+              listeners = {}
+              }: BUILD_OPTIONS<T> = {}): Promise<T> {
 
-if( ! initialize && parent === null)
-throw new Error("A parent must be given if initialize is false");
+  if( ! initialize && parent === null)
+    throw new Error("A parent must be given if initialize is false");
 
-let CustomClass = await customElements.whenDefined(tagname);
-let elem = new CustomClass(params) as LISSHost<T>;
+  let CustomClass = await customElements.whenDefined(tagname);
+  let elem = new CustomClass(params) as LISSHost<T>;
 
-// Fix issue #2
-if( elem.tagName.toLowerCase() !== tagname )
-elem.setAttribute("is", tagname);
+  // Fix issue #2
+  if( elem.tagName.toLowerCase() !== tagname )
+  elem.setAttribute("is", tagname);
 
-if( id !== undefined )
-elem.id = id;
+  if( id !== undefined )
+  elem.id = id;
 
-if( classes.length > 0)
-elem.classList.add(...classes);
+  if( classes.length > 0)
+  elem.classList.add(...classes);
 
-for(let name in cssvars)
-elem.style.setProperty(`--${name}`, cssvars[name]);
+  for(let name in cssvars)
+  elem.style.setProperty(`--${name}`, cssvars[name]);
 
-for(let name in attrs) {
+  for(let name in attrs) {
 
-let value = attrs[name];
-if( typeof value === "boolean")
-elem.toggleAttribute(name, value);
-else
-elem.setAttribute(name, value);
-}
+  let value = attrs[name];
+  if( typeof value === "boolean")
+  elem.toggleAttribute(name, value);
+  else
+  elem.setAttribute(name, value);
+  }
 
-for(let name in data) {
+  for(let name in data) {
 
-let value = data[name];
-if( value === false)
-delete elem.dataset[name];
-else if(value === true)
-elem.dataset[name] = "";
-else
-elem.dataset[name] = value;
-}
+  let value = data[name];
+  if( value === false)
+  delete elem.dataset[name];
+  else if(value === true)
+  elem.dataset[name] = "";
+  else
+  elem.dataset[name] = value;
+  }
 
-if( ! Array.isArray(content) )
-content = [content as any];
-elem.replaceChildren(...content);
+  if( ! Array.isArray(content) )
+  content = [content as any];
+  elem.replaceChildren(...content);
 
-for(let name in listeners)
-elem.addEventListener(name, listeners[name]);
+  for(let name in listeners)
+  elem.addEventListener(name, listeners[name]);
 
-if( parent !== undefined )
-parent.append(elem);
+  if( parent !== undefined )
+  parent.append(elem);
 
-if( ! elem.isInit && initialize )
-return await LISS.initialize(elem);
+  if( ! elem.isInit && initialize )
+  return await LISS.initialize(elem);
 
-return await LISS.getLISS(elem);
+  return await LISS.getLISS(elem);
 }
 LISS.build = build;
 
@@ -196,3 +197,4 @@ LISS.initializeSync(elem);
 return LISS.getLISSSync(elem);
 }
 LISS.buildSync = buildSync;
+*/
