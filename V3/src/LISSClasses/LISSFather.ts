d12e4c4b3eb07486a7ab98b8b58cc192a5e43984
denmigda <denis.migdal@uca.fr>
2025-02-26 19:23:52 +0100

    keep track of children in LISSFather

--- V3/src/LISSClasses/LISSFather.ts
+++ V3/src/LISSClasses/LISSFather.ts
@@ -3,37 +3,58 @@ import LISSUpdate from "./LISSUpdate";
 
 export const WAITING_UPGRADE = Symbol();
 
-const obsChildren = new MutationObserver( (records: MutationRecord[]) => {
-			
-    for(let i = 0; i < records.length; ++i) {
-        const {addedNodes, removedNodes} = records[i];
-        const target: LISSFather = records[i].target as any;
-
-        for(let j = 0; j < addedNodes.length; ++j) {
-            const node = addedNodes[j]
-            if( node instanceof LISSChild )
-                (target as any).onAttach( node );
-            else
-                (node as any)[WAITING_UPGRADE] = true;
-        }
-
-        for(let j = 0; j < removedNodes.length; ++j) {
-            const node = removedNodes[j]
-            if( node instanceof LISSChild )
-                (target as any).onDetach( node );
-            else
-                (node as any)[WAITING_UPGRADE] = false;
-        }
-    }
-});
+function observe( target: LISSFather, callback: (records: MutationRecord[]) => void ) {
+    new MutationObserver(callback).observe(target, {childList: true});
+}
 
 // extends LISSSignal (?) Properties merger (?)
 export default class LISSFather extends LISSUpdate {
 
+    protected LISSChildren = new Array<LISSChild>();
+
     constructor() {
         super();
 
-        obsChildren.observe(this, {childList: true});
+        observe(this, (records: MutationRecord[]) => {
+
+            this.updateChildrenList();
+
+            for(let i = 0; i < records.length; ++i) {
+                this.processAddedNodes  (records[i].addedNodes);
+                this.processRemovedNodes(records[i].removedNodes);                
+            }
+        });
+
+        this.updateChildrenList();
+        this.processAddedNodes(this.childNodes);
+    }
+
+    protected processAddedNodes(nodes: NodeList) {
+
+        for(let j = 0; j < nodes.length; ++j) {
+            const node = nodes[j]
+            if( node instanceof LISSChild )
+                this.onAttach( node );
+            else
+                (node as any)[WAITING_UPGRADE] = true;
+        }
+    }
+
+    protected processRemovedNodes(nodes: NodeList) {
+
+        for(let j = 0; j < nodes.length; ++j) {
+            const node = nodes[j]
+            if( node instanceof LISSChild )
+                this.onDetach( node );
+            else
+                (node as any)[WAITING_UPGRADE] = false;
+        }
+    }
+
+    protected updateChildrenList() {
+        this.LISSChildren = [ ...this.children ].filter( (e) => {
+            return e instanceof LISSChild
+        })
     }
 
     protected onDetach(child: LISSChild) {
