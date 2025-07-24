ac17b5f83999abf686fcc6e9866c04d2dc8c4994
denmigda <denis.migdal@uca.fr>
2025-07-24 17:05:21 +0200

    Improve code-script

--- V3/components/code/code-script/index.ts
+++ V3/components/code/code-script/index.ts
@@ -27,47 +27,54 @@ export function keepSpaces(code: string) {
     return code;
 }
 
+export function raw2html(code: string, codeLang: string) {
+
+    const replaced: string[] = [];
+    code = code.replaceAll(/\<h\>(.*?)\<\/h\>/g, (_, match) => {
+        replaced.push(match);
+        return `__${replaced.length-1}__`
+    });
+
+    code = hl(code, codeLang);
+
+    code = code.replaceAll(/__([\d]*)__/g, (_, match) => {
+
+        let content = replaced[+match];
+        content = content.replaceAll(/(\$[\w_]*)/g, (_, match) => {
+            return `<var>${match}</var>`
+        });
+
+        return `<h>${content}</h>`;
+    });
+
+    return keepSpaces(code); // due to stupid FF bug.
+}
+
 export default class Script extends LISS({ css: [theme, css] },
                             WithBare, WithContent) {
 
     constructor(code?: string, codeLang?: string) {
         super();
 
-        code     ??= this.host.textContent!;
+        if( code === undefined )
+            code = this.host.textContent!;
+        else {
+            this.host.textContent = code;
+        }
+
         codeLang ??= this.host.getAttribute("code-lang") ?? "text";
 
         if(code[0] === '\n') {
             this.host.classList.toggle("block", true);
-
             code = unindent(code);
         }
 
-        // ...
-        const replaced: string[] = [];
-        code = code.replaceAll(/\<h\>(.*?)\<\/h\>/g, (_, match) => {
-            replaced.push(match);
-            return `__${replaced.length-1}__`
-        });
-    
         if(codeLang === "html") {
             code = code.replace("<xbody>", "</body>");
             code = code.replace("<xscript>", "</script>");
         }
 
-        code = hl(code, codeLang);
-
-        code = code.replaceAll(/__([\d]*)__/g, (_, match) => {
-
-            let content = replaced[+match];
-            content = content.replaceAll(/(\$[\w_]*)/g, (_, match) => {
-                return `<var>${match}</var>`
-            });
-
-            return `<h>${content}</h>`;
-        });
-
-        this.content.innerHTML = keepSpaces(code); // due to stupid FF bug.
-        // use a div for FF ?
+        this.content.innerHTML = raw2html(code, codeLang);
     }
 }
 
